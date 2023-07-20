ce89440a47616f0813507ede75b8e8b02809175b
Shandybroom <shandybroom@gmail.com>
2023-07-20 14:14:26 +0700

    add layout

---
+++ resources/views/landing-page.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Honey Mom & Baby Spa</title>
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+</head>
+<body>
+    <h1>Baby Spa Logo</h1>
+</body>
+</html>
